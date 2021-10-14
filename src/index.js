d3068e7abc78aa477f4c8847eba38814a083153a
Kobus <kobus@infinityarc.net>
2021-10-14 14:20:42 +0200

    added methods

--- src/index.js
+++ src/index.js
@@ -5,11 +5,30 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('pages'))
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
+});
+
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// app.use(upload.array());
 app.use(express.static('pages'));
 
+app.all('*', function(req, res, next) {
+    console.log(res.statusCode)
+    console.log(req.path)
+    console.log(req.host)
+    console.table(req.headers)
+    next()
+})
+
+app.post('/api/details', function(req, res) {
+    console.log(req.body);
+    res.send('Thank you for submitting your details!')
+})
 
 
 app.listen(port, () => {
