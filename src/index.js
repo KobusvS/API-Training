6aecfb1cc20805fb4786bcd596a6a955e59d6ef8
Kobus <kobus@infinityarc.net>
2021-10-14 11:17:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use(express.static('pages'));
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`)
+});
