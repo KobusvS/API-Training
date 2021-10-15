fab8044d45fa4a34469c0c4ddae8d8461357678d
Kobus <kobus@infinityarc.net>
2021-10-15 14:26:55 +0200

    new commit

--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,13 @@
-
 const { check } = require('express-validator');
 
 const loginValidate = () => [
-    check('name_field', 'Name should not contain numbers or special characters').trim(),
-    check('email_field', 'Enter a valid email address')
+    check('name', 'Name should not contain numbers or special characters').trim(),
+    check('email', 'Enter a valid email address')
     .isEmail()
     .trim()
     .escape()
     .normalizeEmail(),
-    check('password_field')
+    check('password')
     .isLength({ min: 8 })
     .withMessage('Password must be at least 8 characters long')
     .matches('[0-9]')
@@ -16,17 +15,7 @@ const loginValidate = () => [
     .matches('[A-Z]')
     .withMessage('Password must contain an uppercase letter')
     .trim()
-    .escape(),
-    check('confirm_password_field')
-    .custom(() => {
-        if (req.body.password_field === req.body.confirm_password_field) {
-            return true;
-        } else {
-            return false;
-        }
-    })
-    .withMessage('Passwords do not match')
-
+    .escape()
 ];
 
 module.exports = loginValidate;
